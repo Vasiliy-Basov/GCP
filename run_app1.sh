ec2e16683a0d190538c53eee3e45020a7adca99e
Vasiliy Basov <117505159+Vasiliy-Basov@users.noreply.github.com>
2022-11-15 21:58:48 +0300

    Update run_app1.sh

--- run_app1.sh
+++ run_app1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/Vasiliy-Basov/GCP/main/install_ruby.sh -O
-wget https://raw.githubusercontent.com/Vasiliy-Basov/GCP/main/install_mnogodb.sh -O
-wget https://raw.githubusercontent.com/Vasiliy-Basov/GCP/main/deploy.sh -O
+wget -O - https://raw.githubusercontent.com/Vasiliy-Basov/GCP/main/install_ruby.sh | bash
+wget -O - https://raw.githubusercontent.com/Vasiliy-Basov/GCP/main/install_mnogodb.sh | bash
+wget -O - https://raw.githubusercontent.com/Vasiliy-Basov/GCP/main/deploy.sh | bash
