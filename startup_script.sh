73b962972c180432e12c80970e43f9a34b80356a
Vasiliy Basov <117505159+Vasiliy-Basov@users.noreply.github.com>
2022-11-16 15:06:21 +0300

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -3,13 +3,13 @@
 apt update
 apt install -y ruby-full ruby-bundler build-essential
 # Installing Mongodb
-curl -fsSL https://www.mongodb.org/static/pgp/server-3.6.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+curl -fsSL https://www.mongodb.org/static/pgp/server-3.6.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/3.6 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 # tee — команда, выводит на экран, или же перенаправляет выходной материал команды и копирует его в файл или в переменную
-sudo apt update
-sudo apt install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+apt update
+apt install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
 # Since instances run startup scripts as root, and we don't want our web-server to have such privileges,
 # we need to use either runuser or su to workaround that.
 # For reference:
