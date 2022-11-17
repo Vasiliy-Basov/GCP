1c475d1903ef36c9b727d008218a56539024632e
Vasiliy Basov <vasiliy.basov.82@gmail.com>
2022-11-17 22:09:56 +0300

    New 1604 Files

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
-cd /home/baggurd
-git clone -b monolith https://github.com/express42/reddit.git
-cd reddit && bundle install
-puma -d
+# Since instances run startup scripts as root, and we don't want our web-server to have such privileges,
+# we need to use either runuser or su to workaround that.
+# For reference:
+# https://www.cyberciti.biz/open-source/command-line-hacks/linux-run-command-as-different-user/
+runuser -l baggurd -c 'git clone -b monolith https://github.com/express42/reddit.git'
+runuser -l baggurd -c 'cd reddit && bundle install'
+runuser -l baggurd -c 'cd reddit && puma -d'
